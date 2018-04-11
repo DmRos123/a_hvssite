b3c106084d8c10831862382dd542f37b039b34d6
Dmros123 <d.rosenthal@ymail.com>
2018-04-11 13:43:22 -0700

    initial commit for content files

---
+++ wp-content/themes/genesis-sample/front-page.php
@@ -0,0 +1,61 @@
+<?php
+
+add_action( 'genesis_meta', 'my_homepage_setup' );
+/**
+ * Add widget support for homepage.
+ *
+ */
+function my_homepage_setup() {
+    
+    $home_sidebars = array(
+        'front_page_1'  => is_active_sidebar( 'front-page-1'),
+        'front_page_2'  => is_active_sidebar( 'front-page-2'),
+        'front_page_3'  => is_active_sidebar( 'front-page-3'),
+    );
+
+	if ( $home_sidebars['front_page_1'] )  {
+
+		 //* Add front page 1 widget
+		add_action( 'genesis_after_header', 'display_front_page_1_widget' );
+
+	}
+    if ( $home_sidebars['front_page_2']  )  {
+
+		 //* Add front page 2 widget
+		add_action( 'genesis_before_content_sidebar_wrap', 'display_front_page_2_widget' );
+
+	}
+    if ( $home_sidebars['front_page_3']  )  {
+
+		 //* Add front page 3 widget
+		add_action( 'genesis_after_entry', 'display_front_page_3_widget' );
+
+	}
+}
+
+// Add markup to display front page widgets.
+function display_front_page_1_widget() {
+
+	genesis_widget_area( 'front-page-1', array(
+		'before' => '<div class="front-page-1-widget"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+}
+function display_front_page_2_widget() {
+
+	genesis_widget_area( 'front-page-2', array(
+		'before' => '<div class="front-page-2-widget"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+}
+function display_front_page_3_widget() {
+
+	genesis_widget_area( 'front-page-3', array(
+		'before' => '<div class="front-page-3-widget"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+}
+genesis();
