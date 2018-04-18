c2da2efb1fcd16023f6feefac5f8772c14c5d1bf
Dmros123 <d.rosenthal@ymail.com>
2018-04-18 14:58:30 -0700

    add template page_product for info box from advanced custom fields

--- wp-content/themes/genesis-sample/page_product.php
+++ wp-content/themes/genesis-sample/page_product.php
@@ -10,16 +10,42 @@
  * 
  */
 
-//Add our custom loop
 add_action( 'genesis_loop', 'dmr_products_loop' );
 
-
 function dmr_products_loop() {
-	
-	$args = array(
-	''
-	); 
-}
 
+  
+                      
+     if(function_exists('get_field')){     
+     if (get_field('info_box_content')) {
+         echo '<div class="info-box">';
+         echo '<h1>' . get_field('info_box_title') . '</h1>';
+         the_field('info_box_content');
+         
+          $posts = get_field('related_posts');
+           
+          
+     if( $posts ) {
+        echo '<section class=related-list>';
+        echo '<h1>Additional Leak Detectors:</h1>';
+        echo '<ul>';
+        foreach( $posts as $post ):
+            setup_postdata($post); 
+            echo '<li><a href="' . get_the_permalink() . '">';
+            echo '<h3>' . get_the_title() . '</h3>';
+            the_post_thumbnail();
+            the_excerpt();
+            echo '</a></li>';
+        endforeach;
+        echo '</ul>';
+        wp_reset_postdata(); 
+        echo '</section>';
+     }
+         
+         echo '</div>';   
+     }
+     } 
+}
+                          
 // Run the Genesis loop.
 genesis();
