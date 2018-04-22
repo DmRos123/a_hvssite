0fc731a98a146f1fc5774e2e65e9338296fe920b
Dmros123 <d.rosenthal@ymail.com>
2018-04-21 22:10:17 -0700

    put resources custom field into page_product

--- wp-content/themes/genesis-sample/page_product.php
+++ wp-content/themes/genesis-sample/page_product.php
@@ -13,8 +13,6 @@
 add_action( 'genesis_loop', 'dmr_products_loop' );
 
 function dmr_products_loop() {
-
-  
                       
      if(function_exists('get_field')){     
      if (get_field('info_box_content')) {
@@ -45,6 +43,19 @@ function dmr_products_loop() {
          echo '</div>';   
      }
      } 
+}
+
+add_action( 'genesis_before_sidebar_widget_area', 'dmr_resources_sidebar');
+
+function dmr_resoures_sidebar() {
+	
+	if(get_field('resource_title')) {
+		echo '<div class="resource_box">';
+		echo '<h1>' . get_field('resource_title') . '</h1>';
+		the_field('rental_forms_&_warranty');
+		echo '<h2>' . get_field('specifications_and_documents') . '</h2>';
+	}
+
 }
                           
 // Run the Genesis loop.
